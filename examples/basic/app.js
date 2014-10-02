b4b3371d2603fba95784bceb17bcf5713a6b5d5f
Boris Letocha <boris.letocha@gmail.com>
2014-10-02 22:13:15 +0200

    First example and it works for first try. At least in Chrome and IE11.

---
+++ examples/basic/app.js
@@ -0,0 +1,20 @@
+/// <reference path="../../src/bobril.d.ts"/>
+function p() {
+    var args = [];
+    for (var _i = 0; _i < (arguments.length - 0); _i++) {
+        args[_i] = arguments[_i + 0];
+    }
+    return { tag: "p", children: args };
+}
+
+var frame = 0;
+b.init(function () {
+    b.invalidate();
+    frame++;
+    return [
+        { tag: "h1", children: "First Bobril sample" },
+        p("I know, it is a little bit simplistic, but it is a start"),
+        p("Uptime: ", b.uptime().toFixed(0), "ms Frame: ", frame, " FPS:", (frame * 1000 / b.uptime()).toFixed(1))
+    ];
+});
+//# sourceMappingURL=app.js.map
