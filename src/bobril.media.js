bcbb827ed74652f329e7f0c99c7a42e54be8bc82
Boris Letocha <boris.letocha@gmail.com>
2014-11-26 00:26:47 +0100

    Media detection plugin and example - first version.

---
+++ src/bobril.media.js
@@ -0,0 +1,62 @@
+﻿/// <reference path="../src/bobril.d.ts"/>
+/// <reference path="../src/bobril.media.d.ts"/>
+(function (b, window) {
+    var media = null;
+    var breaks = [600, 1024, 1200];
+
+    function emitOnMediaChange(ev, target, node) {
+        media = null;
+        b.invalidate();
+        return false;
+    }
+
+    var events = ["resize", "deviceorientation"];
+    for (var i = 0; i < events.length; i++)
+        b.addEvent(events[i], 100, emitOnMediaChange);
+
+    function accDeviceBreaks(newBreaks) {
+        if (newBreaks != null)
+            breaks = newBreaks;
+        return breaks;
+    }
+
+    var viewport = window.document.documentElement;
+
+    function getMedia() {
+        if (media == null) {
+            var w = window.innerWidth || viewport.clientWidth;
+            var h = window.innerHeight || viewport.clientHeight;
+            var o = window.orientation;
+            var p;
+            if (o != null) {
+                p = (o != 90) && (o != -90);
+            } else {
+                p = h >= w;
+                o = p ? 0 : 90;
+            }
+            var device = 0;
+            if (p) {
+                while (w > breaks[device])
+                    device++;
+            } else {
+                if (h > breaks[0]) {
+                    device++;
+                    while (w > breaks[device])
+                        device++;
+                }
+            }
+            media = {
+                width: w,
+                height: h,
+                orientation: o,
+                deviceCategory: device,
+                portrait: p
+            };
+        }
+        return media;
+    }
+
+    b.getMedia = getMedia;
+    b.accDeviceBreaks = accDeviceBreaks;
+})(b, window);
+//# sourceMappingURL=bobril.media.js.map
