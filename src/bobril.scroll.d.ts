b0aab0ed3183d72c53b7124828fd0561eccc6190
Bobris <Boris.Letocha@gmail.com>
2014-12-05 17:04:06 +0100

    Scroll plugin iface

---
+++ src/bobril.scroll.d.ts
@@ -0,0 +1,8 @@
+﻿interface IBobrilStatic {
+    registerScrollable?(el: Element);
+    unregisterScrollable?(el: Element);
+    addOnScroll?(callback:()=>void);
+    removeOnScroll?(callback:()=>void);
+    isScrollable?(el: Element): { x:boolean; y:boolean };
+}
+// https://github.com/litera/jquery-scrollintoview/blob/master/jquery.scrollintoview.js
