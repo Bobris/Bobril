55fa9803476b71c310eaff0666ce2854d2cf899b
Boris Letocha <boris.letocha@gmail.com>
2014-12-27 00:42:45 +0100

    Better size info in gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var gulpif = require('gulp-if');
+var fs = require('graceful-fs');
 
 var sources = ['src/**/*.js'];
 var testSources = ['test/**/*.js'];
@@ -12,27 +13,14 @@ var dist = './dist/';
 
 gulp.task('uglify', ['copydef'], function() {
   var uglify = require('gulp-uglify');
-  var bytediff = require('gulp-bytediff');
   var rename = require('gulp-rename');
 
   return gulp.src(sources)
-    .pipe(bytediff.start())
     .pipe(uglify({ compress: { unsafe:true, pure_funcs: [ 'assert' ], global_defs: { DEBUG: false } } }))
-    .pipe(bytediff.stop())
 	.pipe(rename(function (path) {
         path.basename += ".min";
     }))
     .pipe(gulp.dest(dist));
-});
-
-gulp.task('gzipsize', ['uglify'], function() {
-  var size = require('gulp-size');
-  var concat = require('gulp-concat');
-  return gulp.src([dist+'*.js'])
-	.pipe(size({ showFiles: true, gzip:true, title: "Minified+gzip" }))
-	.pipe(concat('all.min.js'))
-	.pipe(size({ showFiles: false, gzip:true, title: "Minified+concat+gzip" }))
-	;
 });
 
 gulp.task('copydef', function() {
@@ -40,7 +28,7 @@ gulp.task('copydef', function() {
     .pipe(gulp.dest(dist));
 });
 
-gulp.task('dist', ['copydef','uglify','gzipsize','sloc']);
+gulp.task('dist', ['copydef','uglify','calc']);
 
 gulp.task('watch', ['dist'], function() {
   gulp.watch(sources, function() {
@@ -89,6 +77,72 @@ gulp.task('pages', ['buildexamplesbobril'], function () {
     var deploy = require('gulp-gh-pages');
     return gulp.src('./gh-pages/**/*')
         .pipe(deploy({}));
+});
+
+gulp.task('calc', ['uglify'], function () {
+    var gzipSync = require('browserify-zlib').gzipSync;
+    var distList = fs.readdirSync('dist/');
+	distList = distList.filter(function(name) { return /\.min\.js$/.test(name); });
+	function extractPluginName(name) {
+	    if (name=='bobril') return 'bobril';
+		return name.substr(7);
+	}
+	var name2idx = {};
+	var wholebuf = "";
+	distList = distList.map(function(name,i) {
+       var buf = fs.readFileSync("dist/"+name);
+	   var plname=extractPluginName(name.substr(0,name.length-7));
+	   wholebuf+=buf.toString();
+	   name2idx[plname]=i;
+	   return { name:plname, minname: name, minsize: buf.length, gzipsize:gzipSync(buf.toString()).length };
+    });
+    var srcList = fs.readdirSync('src/');
+	srcList.filter(function(name) { return /\.js$/.test(name); }).forEach(function(name) {
+       var buf = fs.readFileSync("src/"+name);
+	   var plname=extractPluginName(name.substr(0,name.length-3));
+	   if (name2idx[plname]==null) return;
+	   var o=distList[name2idx[plname]];
+	   o.jssize=buf.length;
+	   o.jslines=buf.toString().split(/\r\n|\r|\n/).length;
+	});
+	srcList.filter(function(name) { return /\.ts$/.test(name) && !/\.d\.ts$/.test(name); }).forEach(function(name) {
+       var buf = fs.readFileSync("src/"+name);
+	   var plname=extractPluginName(name.substr(0,name.length-3));
+	   if (name2idx[plname]==null) return;
+	   var o=distList[name2idx[plname]];
+	   o.tssize=buf.length;
+	   o.tslines=buf.toString().split(/\r\n|\r|\n/).length;
+	});
+	distList.sort(function(a,b) {
+	   if (a.name==b.name) return 0;
+	   if (a.name=='bobril') return -1;
+	   if (b.name=='bobril') return 1;
+	   if (a.name<b.name) return -1;
+	   return 1;
+	});
+    function padString(len, str, leftPadded) {
+      if (str == undefined) str = ''; else str=''+str;
+      if (leftPadded) {
+        while (str.length < len) str = ' ' + str;
+      } else {
+        while (str.length < len) str = str + ' ';
+      }
+      return str;
+    }	
+	function logInfo(name,tslines,jssize,minsize,gzipsize) {
+	   console.log(padString(12,name)+' '+padString(5,tslines,true)+' '+padString(6,jssize,true)+' '+padString(6,minsize,true)+' '+padString(6,gzipsize,true));
+    }
+	logInfo('Name','Lines','Orig','Minif','Gzip');
+	var totaltslines=0,totaljssize=0,totalminsize=0,totalgzipsize=0;
+	distList.forEach(function(o) {
+	   totaltslines+=o.tslines;
+	   totaljssize+=o.jssize;
+	   totalminsize+=o.minsize;
+	   totalgzipsize+=o.gzipsize;
+	   logInfo(o.name,o.tslines,o.jssize,o.minsize,o.gzipsize);
+	});
+	logInfo('Total Sum',totaltslines,totaljssize,totalminsize,totalgzipsize);
+	logInfo('Concat','','','',gzipSync(wholebuf).length);
 });
 
 gulp.task('default', ['watch']);
