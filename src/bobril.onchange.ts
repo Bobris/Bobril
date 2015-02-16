58e34c8da41d8c842edb9a7e9427455e49c144f3
Bobris <Boris.Letocha@gmail.com>
2015-02-16 13:44:47 +0100

    Fix for iPhone not firing oninput for comboboxes

--- src/bobril.onchange.ts
+++ src/bobril.onchange.ts
@@ -164,7 +164,7 @@
         return false;
     }
 
-    var events = ["input", "cut", "paste", "keydown", "keypress", "keyup", "click"];
+    var events = ["input", "cut", "paste", "keydown", "keypress", "keyup", "click", "blur"];
     for (var i = 0; i < events.length; i++)
-        b.addEvent(events[i], 100, emitOnChange);
+        b.addEvent(events[i], 10, emitOnChange);
 })(b);
