3c5864f0b185df180c1cc26720bddcb958adb5d0
Boris Letocha <boris.letocha@gmail.com>
2015-05-08 00:51:42 +0200

    background gradient shim for Android 4.3- and iOS 6.1.

--- src/bobril.styleshim.js
+++ src/bobril.styleshim.js
@@ -1,2 +1,26 @@
 /// <reference path="bobril.d.ts"/>
-// It was needed only for IE8
+(function (b) {
+    var teststyle = document.createElement("div").style;
+    teststyle.cssText = "background:-webkit-linear-gradient(top,red,red)";
+    if (teststyle.background.length > 0) {
+        var startsWithGradient = /^(?:repeating\-)?(?:linear|radial)\-gradient/ig;
+        var revdirs = { top: "bottom", bottom: "top", left: "right", right: "left" };
+        function gradientWebkitter(style, value, name) {
+            if (startsWithGradient.test(value)) {
+                var pos = value.indexOf("(to ");
+                if (pos > 0) {
+                    pos += 4;
+                    var posend = value.indexOf(",", pos);
+                    var dir = value.slice(pos, posend);
+                    dir = dir.split(" ").map(function (v) { return revdirs[v] || v; }).join(" ");
+                    value = value.slice(0, pos - 3) + dir + value.slice(posend);
+                }
+                value = "-webkit-" + value;
+            }
+            style[name] = value;
+        }
+        ;
+        b.setStyleShim("background", gradientWebkitter);
+        b.setStyleShim("backgroundImage", gradientWebkitter);
+    }
+})(b);
--- src/bobril.styleshim.ts
+++ src/bobril.styleshim.ts
@@ -1,2 +1,26 @@
 /// <reference path="bobril.d.ts"/>
-// It was needed only for IE8
+
+((b: IBobrilStatic) => {
+    var teststyle = document.createElement("div").style;
+    teststyle.cssText = "background:-webkit-linear-gradient(top,red,red)";
+    if (teststyle.background.length > 0) {
+        var startsWithGradient = /^(?:repeating\-)?(?:linear|radial)\-gradient/ig;
+        var revdirs = { top: "bottom", bottom: "top", left: "right", right: "left" };
+        function gradientWebkitter(style: any, value: any, name: string) {
+            if (startsWithGradient.test(value)) {
+                var pos = (<string>value).indexOf("(to ");
+                if (pos > 0) {
+                    pos += 4;
+                    var posend = (<string>value).indexOf(",", pos);
+                    var dir = (<string>value).slice(pos, posend);
+                    dir = dir.split(" ").map(v=> revdirs[v] || v).join(" ");
+                    value = (<string>value).slice(0, pos - 3) + dir + (<string>value).slice(posend);
+                }
+                value = "-webkit-" + value;
+            }
+            style[name] = value;
+        };
+        b.setStyleShim("background", gradientWebkitter);
+        b.setStyleShim("backgroundImage", gradientWebkitter);
+    }
+})(b);
