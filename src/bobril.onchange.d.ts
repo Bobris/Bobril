e4c37688ec9873eb779301bc9fcee51048bf7fdf
m.majoros <m.majoros@gmc.net>
2016-01-29 23:14:06 +0100

    Added support for new events SelectionChange and CaretPositionChange on HTMLInputElements

--- src/bobril.onchange.d.ts
+++ src/bobril.onchange.d.ts
@@ -1,4 +1,15 @@
-﻿interface IBobrilComponent {
+﻿interface ISelectionChangeEvent {
+    startPosition: number;
+    endPosition: number;
+}
+
+interface ICaretPositionChangeEvent {
+    position: number;
+}
+
+interface IBobrilComponent {
     // called on input element after any change with new value (string|boolean)
-    onChange? (ctx: Object, value: any): void;
+    onChange?(ctx: Object, value: any): void;
+    onSelectionChange?(ctx: IBobrilCtx, event: ISelectionChangeEvent): void;
+    onCaretPositionChange?(ctx: IBobrilCtx, event: ICaretPositionChangeEvent): void;
 }
