a12fe164a48d51e7b12f52872372647d815ea615
p.kratochvil <p.kratochvil@quadient.com>
2020-04-14 15:47:34 +0200

    add DOM level events

--- package/spec/events.spec.tsx
+++ package/spec/events.spec.tsx
@@ -32,65 +32,81 @@ describe("events", ()=> {
         b.syncUpdate();
     });
 
-    it("portal event bubbling", () => {
-        const callStack = [];
-        const callback = jasmine.createSpy("spy");
-        callback.and.callFake((arg: string) => callStack.push(arg));
-        let cacheNode;
+    describe("portal", () => {
+        let callStack;
+        let callback: jasmine.Spy;
+        let cacheNode: b.IBobrilCacheNode;
+        beforeEach(() => {
+            callStack = [];
+            callback = jasmine.createSpy("spy");
+            callback.and.callFake((arg: string) => callStack.push(arg));
 
-        interface IProps {
-            callback: (id: string) => void;
-        }
+            interface IProps {
+                callback: (id: string) => void;
+            }
 
-        function PortalWrapper(props: IProps) {
-            const [el, setEl] = b.useState<Element|null>(null);
-            const component = <Portal callback={props.callback}/>
-            b.useEvents({
-                onClick(): b.GenericEventResult {
-                    props.callback("parent");
-                    return false;
-                }
+            function PortalWrapper(props: IProps) {
+                const [el, setEl] = b.useState<Element|null>(null);
+                const component = <Portal callback={props.callback}/>
+                b.useEvents({
+                    onClick(): b.GenericEventResult {
+                        props.callback("parent");
+                        return false;
+                    }
+                });
+                b.useEffect(() => {
+                    setEl(document.getElementById("portal"));
+                },[]);
+
+                return (
+                    <div>
+                        {el && <b.Portal element={el}>
+                            {component}
+                        </b.Portal>}
+                    </div>
+                )
+            }
+
+            function Portal(props: IProps) {
+                return (
+                    <div onClick={() => {
+                        props.callback("inner");
+                        return false
+                    }} ref={ref => cacheNode = ref!}>
+                        Portal
+                    </div>
+                )
+            }
+
+            b.init(() => {
+                return (
+                    <div>
+                        <PortalWrapper callback={callback}/>
+                        <div id="portal" onClick={() => callback("portal")}/>
+                    </div>
+                )
             });
-            b.useEffect(() => {
-                setEl(document.getElementById("portal"));
-            },[]);
-
-            return (
-                <div>
-                    {el && <b.Portal element={el}>
-                        {component}
-                    </b.Portal>}
-                </div>
-            )
-        }
-
-        function Portal(props: IProps) {
-            return (
-                <div onClick={() => {
-                    props.callback("inner");
-                    return false
-                }} ref={ref => cacheNode = ref}>
-                    Portal
-                </div>
-            )
-        }
-
-        b.init(() => {
-            return (
-                <div>
-                    <PortalWrapper callback={callback}/>
-                    <div id="portal" onClick={() => callback("portal")}/>
-                </div>
-            )
+            // first render
+            b.syncUpdate();
+            // second render for portal
+            b.syncUpdate();
         });
-        // first render
-        b.syncUpdate();
-        // second render for portal
-        b.syncUpdate();
-        b.bubble(cacheNode, "onClick");
-        expect(callback).toHaveBeenCalledTimes(2);
-        const calls = callback.calls.all();
-        expect(calls[0].args).toEqual(["inner"]);
-        expect(calls[1].args).toEqual(["parent"]);
+
+        it("portal event bubbling - VDOM", () => {
+            b.bubble(cacheNode, "onClick");
+            expect(callback).toHaveBeenCalledTimes(2);
+            const calls = callback.calls.all();
+            expect(calls[0].args).toEqual(["inner"]);
+            expect(calls[1].args).toEqual(["parent"]);
+        });
+
+        it("portal event bubbling - DOM", () => {
+            const domEl = document.getElementById("inner-portal")!;
+            domEl.click();
+            expect(callback).toHaveBeenCalledTimes(2);
+            const calls = callback.calls.all();
+            expect(calls[0].args).toEqual(["inner"]);
+            expect(calls[1].args).toEqual(["parent"]);
+        });
     });
 });
